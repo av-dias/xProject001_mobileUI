d2e37701e8b7bb120061eb641f02e003109b43e1
av.dias <av.dias@campus.fct.unl.pt>
2023-12-02 16:53:44 +0000

    [RN] Setting up app ts with user navigation and SafeAreaProvider

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { SafeAreaProvider, SafeAreaView, initialWindowMetrics } from "react-native-safe-area-context";
+
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+/* function Tabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: {
+          height: 50,
+          paddingHorizontal: 5,
+          paddingTop: 5,
+          backgroundColor: "black",
+          position: "absolute",
+          borderTopWidth: 0,
+          navigationBarColor: "gold",
+        },
+        navigationBarColor: "gold",
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false, tabBarIcon: ({ color, size }) => <MaterialIcons name="dashboard" size={24} color="white" /> }}
+      />
+    </Tab.Navigator>
+  );
+} */
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <Stack.Navigator>
+          {/* <Stack.Screen name="Login" component={LoginPage} options={{ headerShown: false, orientation: "portrait" }} />
+          <Stack.Screen name="Tabs" component={Tabs} options={{ headerShown: false, orientation: "portrait" }} /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
