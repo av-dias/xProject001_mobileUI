94ce5135113df562f198a1c08338a183df6aea88
av.dias <av.dias@campus.fct.unl.pt>
2023-12-03 21:17:48 +0000

    [Component] Image Container

---
+++ components/imageContainer/ImageContainer.tsx
@@ -0,0 +1,11 @@
+import { View, Image } from "react-native";
+import { ReactNode } from "react";
+
+type ImageSourcePropType = React.ComponentProps<typeof Image>["source"];
+type Props = { imageSrc: ImageSourcePropType };
+
+const ImageContainer: React.FC<Props> = (props) => {
+  return <Image source={props.imageSrc} style={{ width: "auto", height: "100%", borderRadius: 20 }} />;
+};
+
+export default ImageContainer;
