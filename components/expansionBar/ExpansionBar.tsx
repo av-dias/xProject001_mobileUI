6113d78d904d07b04e9f7af45f3016a434c2cab8
av.dias <av.dias@campus.fct.unl.pt>
2023-12-04 22:56:52 +0000

    [Home] Icon Filter Bar

--- components/expansionBar/ExpansionBar.tsx
+++ components/expansionBar/ExpansionBar.tsx
@@ -1,15 +1,16 @@
 import { View, Pressable } from "react-native";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 import IconContainer from "../iconContainer";
 
 type Props = {
   children: ReactNode;
+  onPress: () => void;
 };
 
 const ExpansionBar: React.FC<Props> = (props) => {
   return (
-    <Pressable style={{ width: 60, backgroundColor: "gray", borderRadius: 50, justifyContent: "center" }}>
+    <Pressable onPress={props.onPress} style={{ width: 50, backgroundColor: "lightgray", borderRadius: 20, justifyContent: "center" }}>
       <IconContainer>{props.children}</IconContainer>
     </Pressable>
   );
--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -1,13 +1,14 @@
 import { View, Text, ScrollView } from "react-native";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { SafeAreaProvider, SafeAreaView, initialWindowMetrics } from "react-native-safe-area-context";
-import { AntDesign, Feather, Entypo } from "@expo/vector-icons";
+import { AntDesign, Feather, Entypo, MaterialIcons, Ionicons } from "@expo/vector-icons";
 
 import UsableScreen from "../../components/usableScreen";
 import FilterBar from "../../components/filterBar";
 import InputBox from "../../components/InputBox";
 import ContentBox from "../../components/contentBox";
 import ExpansionBar from "../../components/expansionBar";
+import IconContainer from "../../components/iconContainer";
 
 let Image1 = require("../../assets/images/image1.jpg");
 let Image2 = require("../../assets/images/image2.jpg");
@@ -18,19 +19,63 @@ type PropsWithChildren = {
 };
 
 const Home: React.FC<PropsWithChildren> = ({ navigation }) => {
+  const [showIconFilter, setShowIconFilter] = useState(false);
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <UsableScreen>
         <View style={{ flexDirection: "row", gap: 10 }}>
           <View style={{ flex: 1 }}>
             <FilterBar>
-              <InputBox placeholder="Search..." icon={<AntDesign name="search1" size={24} color="black" />} />
-              <InputBox placeholder="Where..." icon={<Feather name="map-pin" size={24} color="black" />} />
+              {!showIconFilter ? (
+                <>
+                  <InputBox placeholder="Search..." icon={<AntDesign name="search1" size={24} color="black" />} />
+                  <InputBox placeholder="Where..." icon={<Feather name="map-pin" size={24} color="black" />} />
+                </>
+              ) : (
+                <View style={{ flex: 1, borderRadius: 20, overflow: "hidden" }}>
+                  <ScrollView
+                    horizontal={true}
+                    showsHorizontalScrollIndicator={false}
+                    style={{ borderRadius: 20, overflow: "hidden" }}
+                    contentContainerStyle={{ gap: 20, borderRadius: 20, overflow: "hidden" }}
+                  >
+                    <ExpansionBar onPress={() => {}}>
+                      <MaterialIcons name="sports-soccer" size={24} color="black" />
+                    </ExpansionBar>
+                    <ExpansionBar onPress={() => {}}>
+                      <MaterialIcons name="sports-esports" size={24} color="black" />
+                    </ExpansionBar>
+                    <ExpansionBar onPress={() => {}}>
+                      <MaterialIcons name="photo-camera" size={24} color="black" />
+                    </ExpansionBar>
+                    <ExpansionBar onPress={() => {}}>
+                      <Ionicons name="trail-sign-outline" size={24} color="black" />
+                    </ExpansionBar>
+                    <ExpansionBar onPress={() => {}}>
+                      <MaterialIcons name="sports-soccer" size={24} color="black" />
+                    </ExpansionBar>
+                    <ExpansionBar onPress={() => {}}>
+                      <MaterialIcons name="sports-esports" size={24} color="black" />
+                    </ExpansionBar>
+                    <ExpansionBar onPress={() => {}}>
+                      <MaterialIcons name="photo-camera" size={24} color="black" />
+                    </ExpansionBar>
+                    <ExpansionBar onPress={() => {}}>
+                      <Ionicons name="trail-sign-outline" size={24} color="black" />
+                    </ExpansionBar>
+                  </ScrollView>
+                </View>
+              )}
+              <ExpansionBar
+                onPress={() => {
+                  setShowIconFilter(!showIconFilter);
+                }}
+              >
+                <Entypo name="sound-mix" size={24} color="black" />
+              </ExpansionBar>
             </FilterBar>
           </View>
-          <ExpansionBar>
-            <Entypo name="sound-mix" size={24} color="black" />
-          </ExpansionBar>
         </View>
 
         <ScrollView style={{ backgroundColor: "transparent", gap: 20 }} contentContainerStyle={{ gap: 20 }}>
