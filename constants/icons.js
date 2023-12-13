8612484bcac8687be0e45b7295c952fbd9dbc294
av.dias <av.dias@campus.fct.unl.pt>
2023-12-13 21:08:15 +0000

    [Constant] icons

---
+++ constants/icons.js
@@ -0,0 +1,19 @@
+import { AntDesign, MaterialIcons, Ionicons } from "@expo/vector-icons";
+
+import key from "./key";
+
+const iconsfilter = [
+  { name: key.iconfilter + "sports-soccer", icon: <MaterialIcons name="sports-soccer" size={24} color="black" /> },
+  { name: key.iconfilter + "photo-camera", icon: <MaterialIcons name="photo-camera" size={24} color="black" /> },
+  { name: key.iconfilter + "trail-sign-outline", icon: <Ionicons name="trail-sign-outline" size={24} color="black" /> },
+  { name: key.iconfilter + "sports-soccer2", icon: <MaterialIcons name="sports-soccer" size={24} color="black" /> },
+  { name: key.iconfilter + "photo-camera2", icon: <MaterialIcons name="photo-camera" size={24} color="black" /> },
+  { name: key.iconfilter + "trail-sign-outline2", icon: <Ionicons name="trail-sign-outline" size={24} color="black" /> },
+  { name: key.iconfilter + "sports-soccer3", icon: <MaterialIcons name="sports-soccer" size={24} color="black" /> },
+  { name: key.iconfilter + "photo-camera3", icon: <MaterialIcons name="photo-camera" size={24} color="black" /> },
+  { name: key.iconfilter + "trail-sign-outline3", icon: <Ionicons name="trail-sign-outline" size={24} color="black" /> },
+];
+
+const heartIcon = (size = 40) => <AntDesign name="hearto" size={size} color={color.light.red} />;
+
+export { iconsfilter, heartIcon };
