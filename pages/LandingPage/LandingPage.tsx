7dd42462b2e815153bc65cec223ac85ed5defb3f
av.dias <av.dias@campus.fct.unl.pt>
2023-12-16 19:10:39 +0000

    [Page] Landing Page draft

---
+++ pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,48 @@
+import { View, Text } from "react-native";
+import { ReactNode } from "react";
+import { SafeAreaProvider, SafeAreaView, initialWindowMetrics } from "react-native-safe-area-context";
+import { FontAwesome } from "@expo/vector-icons";
+
+import UsableScreen from "../../components/usableScreen";
+import InputBox from "../../components/InputBox";
+
+type PropsWithChildren = {
+  navigation: any;
+};
+
+const LandingPage: React.FC<PropsWithChildren> = ({ navigation }) => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <UsableScreen>
+        <View style={{ flex: 1, gap: 50 }}>
+          <View style={{ flex: 1, gap: 20 }}>
+            <View style={{ padding: 20, alignItems: "center" }}>
+              <View style={{ width: "50%", height: 150, backgroundColor: "gray" }}></View>
+            </View>
+            <View style={{ height: 50 }}>
+              <InputBox
+                icon={<FontAwesome name="map-pin" size={24} color="black" />}
+                placeholder="Where do you want to stumble upon first?"
+                onSubmitEditing={(data: any) => {
+                  navigation.navigate("Tabs");
+                }}
+              ></InputBox>
+            </View>
+          </View>
+          <View style={{ flex: 2 }}>
+            <View style={{ flex: 1 }}>
+              <View style={{ width: "100%", height: 400, backgroundColor: "gray", justifyContent: "center", gap: 20 }}>
+                <Text style={{ textAlign: "center", justifyContent: "center", fontSize: 20 }}>
+                  The best things in life are found by stumbling upon them.
+                </Text>
+                <Text style={{ textAlign: "center", justifyContent: "center", fontSize: 14 }}>We are here to help</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </UsableScreen>
+    </SafeAreaView>
+  );
+};
+
+export default LandingPage;
