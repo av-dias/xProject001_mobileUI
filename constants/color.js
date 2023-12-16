15bd644363583924e8911d21c1421464f30748e8
av.dias <av.dias@campus.fct.unl.pt>
2023-12-16 19:08:35 +0000

    [Constant] Color update

--- constants/color.js
+++ constants/color.js
@@ -1,4 +1,8 @@
 export default color = {
-  light: { red: "#E34444" },
+  light: {
+    red: "#E34444",
+    primary: "#FFCC80",
+    grayBlur: "rgba(217, 217, 217, 0.5)",
+  },
   dark: { red: "#E34444" },
 };
