eecec4daadf23d252a0a2f8e3d22b566166f7308
av.dias <av.dias@campus.fct.unl.pt>
2023-12-20 20:13:43 +0000

    [TextBox] Enable icon position Left or right

--- components/textBox/TextBox.tsx
+++ components/textBox/TextBox.tsx
@@ -8,15 +8,17 @@ type PropsWithChildren = {
   text: string | number;
   fontWeight?: "normal" | "bold";
   fontSize?: number | 10;
+  iconPosition?: "left" | "right";
 };
 
 const TextBox: React.FC<PropsWithChildren> = (props) => {
   return (
     <View style={{ flexDirection: "row", borderRadius: 15, gap: 5 }}>
-      <IconContainer>{props.icon}</IconContainer>
+      {props.iconPosition == "left" || (props.iconPosition == undefined && <IconContainer>{props.icon}</IconContainer>)}
       <View style={{ justifyContent: "center" }}>
         <Text style={{ fontWeight: props.fontWeight, fontSize: props.fontSize }}>{props.text}</Text>
       </View>
+      {props.iconPosition == "right" && <IconContainer>{props.icon}</IconContainer>}
     </View>
   );
 };
