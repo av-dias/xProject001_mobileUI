05c2f3ee2d56a11bc4dbff5df69d79eb9197a7ef
av.dias <av.dias@campus.fct.unl.pt>
2024-03-08 11:27:47 +0000

    [Home and Map] Polished timetable view

--- pages/Home/handler.tsx
+++ pages/Home/handler.tsx
@@ -5,6 +5,14 @@ import ContentBox from "../../components/contentBox";
 import { activityListHandlerMock } from "../../constants/mockData";
 import { addToStorage, removeFromStorage } from "../../functions/localStorage";
 import storage from "../../constants/storage";
+import { getDayOfWeek } from "../../constants/calendar";
+
+const getTimeFromTimeTable = (time: string) => {
+  let timetableJSON = JSON.parse(time);
+  let todayDay = new Date().getDay();
+  if (timetableJSON[getDayOfWeek(todayDay)].start == "Closed") return timetableJSON[getDayOfWeek(todayDay)].start;
+  else return timetableJSON[getDayOfWeek(todayDay)].start + " - " + timetableJSON[getDayOfWeek(todayDay)].end;
+};
 
 const renderActivityItem = (render: ListRenderItemInfo<ActivityType>, navigation: any, setActivityList: any) => (
   <View style={{ width: "100%", height: 205 }}>
@@ -13,7 +21,7 @@ const renderActivityItem = (render: ListRenderItemInfo<ActivityType>, navigation
       title={render.item.title}
       location={render.item.location}
       price={render.item.price}
-      timetable={render.item.timetable}
+      timetable={getTimeFromTimeTable(render.item.timetable)}
       rate={render.item.rate}
       navigation={() => navigation.navigate("ActivityDetails", render.item)}
       onFavorite={async () => {
--- pages/MapPage/handler.tsx
+++ pages/MapPage/handler.tsx
@@ -3,8 +3,16 @@ import { activityListHandlerMock } from "../../constants/mockData";
 import { ListRenderItemInfo } from "react-native";
 import { ActivityType } from "../../constants/models";
 import ContentBox from "../../components/contentBox";
+import { getDayOfWeek } from "../../constants/calendar";
 
 const markerHandler = activityListHandlerMock;
+
+const getTimeFromTimeTable = (time: string) => {
+  let timetableJSON = JSON.parse(time);
+  let todayDay = new Date().getDay();
+  if (timetableJSON[getDayOfWeek(todayDay)].start == "Closed") return timetableJSON[getDayOfWeek(todayDay)].start;
+  else return timetableJSON[getDayOfWeek(todayDay)].start + " - " + timetableJSON[getDayOfWeek(todayDay)].end;
+};
 
 const renderActivityItem = (render: ListRenderItemInfo<ActivityType>, navigation: any) => (
   <View style={{ width: "100%", height: 205 }}>
@@ -13,7 +21,7 @@ const renderActivityItem = (render: ListRenderItemInfo<ActivityType>, navigation
       title={render.item.title}
       location={render.item.location}
       price={render.item.price}
-      timetable={render.item.timetable}
+      timetable={getTimeFromTimeTable(render.item.timetable)}
       rate={render.item.rate}
       navigation={() => navigation.navigate("ActivityDetails", render.item)}
       favorite={render.item.favorite}
