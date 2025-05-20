4d575ef9e34a58bb78ec18ae0243d24bdca2d702
David Silva <51048697+jxtzzz@users.noreply.github.com>
2025-05-20 15:18:10 +0100

    Refactoring FilterBar and adding the refactored version to Home

--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -112,48 +112,10 @@ const Home: React.FC<PropsWithChildren> = ({ navigation }) => {
         )}
         <View style={{ flexDirection: "row", gap: 10 }}>
           <View style={{ flex: 1 }}>
-            <FilterBar>
-              {!showIconFilter ? (
-                <>
-                  <InputBox
-                    placeholder="Search..."
-                    icon={<AntDesign name="search1" size={24} color="black" />}
-                  />
-                  <InputBox
-                    placeholder="Where..."
-                    icon={<Feather name="map-pin" size={24} color="black" />}
-                  />
-                </>
-              ) : (
-                <View style={{ flex: 1, borderRadius: 20, overflow: "hidden" }}>
-                  <ScrollView
-                    horizontal={true}
-                    showsHorizontalScrollIndicator={false}
-                    style={{ borderRadius: 20, overflow: "hidden" }}
-                    contentContainerStyle={{
-                      gap: 20,
-                      borderRadius: 20,
-                      overflow: "hidden",
-                    }}
-                  >
-                    {iconsfilter.map((filter) => {
-                      return (
-                        <ExpansionBar key={filter.name} onPress={() => {}}>
-                          {filter.icon}
-                        </ExpansionBar>
-                      );
-                    })}
-                  </ScrollView>
-                </View>
-              )}
-              <ExpansionBar
-                onPress={() => {
-                  setShowIconFilter(!showIconFilter);
-                }}
-              >
-                <Entypo name="sound-mix" size={24} color="black" />
-              </ExpansionBar>
-            </FilterBar>
+            <FilterBar
+              setShowIconFilter={setShowIconFilter}
+              showIconFilter={showIconFilter}
+            ></FilterBar>
           </View>
         </View>
         <FlatList
